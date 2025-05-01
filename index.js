4cb3f3b2639ee8ddfbec496730f2ef0712d6e143
Deku504 <millapadilla5@gmail.com>
2025-05-01 03:20:02 -0500

    index.js

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ const app = express();
 app.use(bodyParser.json());
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert({
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    type: "service_account",
+  }),
 });
 
 app.post("/send-notification", async (req, res) => {
-  const message = req.body;
+  const message = req.body.message; // Asegúrate que el JSON tenga key "message"
 
   try {
     const response = await admin.messaging().send(message);
